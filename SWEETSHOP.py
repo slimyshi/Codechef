4589573a545e4b054f7430acb45b1982be6098e3
Satvik vyas <167942889+slimyshi@users.noreply.github.com>
2026-01-30 11:16:54 +0530

    Update SWEETSHOP.py

--- SWEETSHOP.py
+++ SWEETSHOP.py
@@ -1,17 +1,14 @@
 # cook your dish here
 # Input: initial amount X and number of laddus N
-X = int(input())  # Initial amount of money
-N = int(input())  # Number of laddus bought
+X = int(input()) 
+N = int(input()) 
 
-# Each laddu costs 10 and each jalebi costs 20
 cost_laddu = 10
 cost_jalebi = 20
 
-# Calculate the remaining money after buying N laddus
 remaining_money = X - (cost_laddu * N)
 
-# Calculate how many jalebis can be bought with the remaining money
 jalebis = remaining_money // cost_jalebi
 
-# Output the number of jalebis
+
 print(jalebis)
